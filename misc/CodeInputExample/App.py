4fc158efaf8fafcf0869014e4deb140f68992a12
PratikGarai <pratikgarai0208@gmail.com>
2020-08-29 20:25:47 +0530

    Done for now

--- misc/CodeInputExample/App.py
+++ misc/CodeInputExample/App.py
@@ -10,14 +10,32 @@ from kivy.uix.relativelayout import RelativeLayout
 class CodeSection(RelativeLayout):
     def __init__(self, **kwargs):
         super(CodeSection, self).__init__(**kwargs)
-        self.pos_hint = {'x':0, 'y':0}
-        self.size_hint = (1,0.9)
         self.add_widget(CodeInput(lexer = KivyLexer()))
+
+class UpperSection(RelativeLayout):
+    def __init__(self, **kwargs):
+        super(UpperSection, self).__init__(**kwargs)
+
+        dropdown = DropDown()
+        dropdown.add_widget(Button(text="1" , size_hint_y=None , height=20))
+        dropdown.add_widget(Button(text="2" , size_hint_y=None , height=20))
+        dropdown.add_widget(Button(text="3" , size_hint_y=None , height=20))
+        dropdown.add_widget(Button(text="4" , size_hint_y=None , height=20))
+
+        MainButton = Button(text = "Open Dropdown")
+        MainButton.bind(on_release = dropdown.open)
+        
+        self.add_widget(MainButton)
+        self.add_widget(dropdown)
+
+        self.pos_hint = {'x':0, 'y':0}
+        self.size_hint = (1,0.1)
 
 class MainLayout(BoxLayout):
     def __init__(self, **kwargs):
         super(MainLayout, self).__init__(**kwargs)
         self.orientation = 'vertical'
+        self.add_widget(UpperSection())
         self.add_widget(CodeSection())
 
 class MainApp(App):
