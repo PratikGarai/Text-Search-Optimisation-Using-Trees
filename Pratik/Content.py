525aa537d4cfdd24cfe11e39ef4fe0f0dee4d638
PratikGarai <pratikgarai0208@gmail.com>
2020-10-11 09:02:39 +0530

    Added the class structure of content part

---
+++ Pratik/Content.py
@@ -0,0 +1,20 @@
+class FullText():
+
+    def __init__(self, text):
+        self.sentences = []
+
+
+class Sentence():
+
+    def __init__(self, sentence):
+        self.words = []
+        self.number = None
+
+
+class Word():
+
+    def __init__(self, word):
+        self.content = []
+        self.number = None
+        self.address_begin = None
+        self.address_end = None
