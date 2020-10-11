4fb8104a2cc1d61ceb006c2f102798e5aa072a0d
PratikGarai <pratikgarai0208@gmail.com>
2020-10-11 18:38:29 +0530

    Added stuff to trie classes

--- Pratik/Trie.py
+++ Pratik/Trie.py
@@ -1,13 +1,34 @@
 class Alphabet():
 
-    def __init__(self, alphabet, level, is_ending):
+    def __init__(self, alphabet, level):
         self.alphabet = alphabet
         self.children = {}
-        self.is_ending = is_ending
+        self.is_ending = False
         self.level = level
+        self.addresses = []
+
+    def moveToChild(self, character):
+        try :
+            return self.children[character]
+        else :
+            self.children.[character] = Alphabet(character, self.level+1)
+            return self.children[character]
+
+    def setEnding(self):
+        self.is_ending = True
+
+    def addAddress(self, instance):
+        self.addresses.append(instance)
 
 
 class Trie():
 
     def __init__(self):
         self.head = Alphabet('', 0, False)
+
+    def add(self, word, word_pointer):
+        current = self.head
+        for i in word :
+            current = current.moveToChild(i)
+        current.setEnding()
+        current.addAddress(word_pointer)
--- Pratik/tester.py
+++ Pratik/tester.py
@@ -1,14 +1,16 @@
 from Content import FullText
+from Trie import Trie
 
 def main():
-    ob = None
+    ob_text = None
+    ob_trie = Trie()
     with open("test.txt", "r") as t:
         text = t.read()
         print(text)
-        ob = FullText(text)
+        ob_text = FullText(text)
     print("\n Text Added! Now launching printAll() \n")
     print("S.no.\tW.no.\tBeg.\tEnd\tContent")
-    ob.printAll()
+    ob_text.printAll()
 
 if __name__=='__main__':
     main()
