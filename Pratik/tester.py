90510240496e9a3d83e94e5a0e5f857654f9b7cf
PratikGarai <pratikgarai0208@gmail.com>
2020-10-13 13:47:07 +0530

    Fully automatic testing is ready

--- Pratik/tester.py
+++ Pratik/tester.py
@@ -4,15 +4,16 @@ from Document import Document
 from LinearModel import LinearModel
 import time
 
-def test(f_name):
+def test(f_name, s_queries, a_queries):
 
     # declarations of the structures
     doc = None
+    lin = None
 
     # reading an inputing the file
     with open(f_name, "r") as t:
         text = t.read()
-        print(text)
+        # print(text)
         doc = Document(text)
         lin = LinearModel(text)
 
@@ -34,35 +35,51 @@ def test(f_name):
         else:
             print("Node doesn't exist")
 
-    print("Number of search queries : ", end = "")
-    n = int(input())
-    for i in range(n):
-        print("Query",(i+1),": ", end="")
-        q = input()
-        print("\nDocument model results\n------------")
+    print("Search queries : ")
+    for i in s_queries:
+        print("Query : ", i)
+        print("Document model results\n------------")
         a = time.time()
-        res1 = doc.search(q)
-        search_results_printer(res1)
+        res1 = doc.search(i)
         b = time.time()
+        search_results_printer(res1)
         print("Time : ",b-a)
         print("\nLinear model results\n------------")
         c = time.time()
-        res2 = lin.search(q)
-        search_results_printer(res2)
+        res2 = lin.search(i)
         d = time.time()
+        search_results_printer(res2)
         print("Time : ",d-c)
 
-    print("Number of autocomplete queries : ", end = "")
-    n = int(input())
-    for i in range(n):
-        print("Query",(i+1),": ", end="")
-        q = input()
-        res = doc.suggestions(q)
+    print("\n-----\nAutocomplete queries : ")
+    for i in a_queries:
+        print("Query : ", i)
+        res = doc.suggestions(i)
         if res!=[]:
             print("Suggestions ", res)
         else:
             print("No suggestions")
 
+    # for the purpose of storage comparison
+    return [lin, doc]
+
+class Test():
+    def __init__(self, name, file_name, s_queries, a_queries):
+        self.name = name
+        self.file_name  = file_name
+        self.s_queries = s_queries
+        self.a_queries = a_queries
+        self.objs = None
+
+    def conduct(self):
+        self.objs = test(self.file_name, self.s_queries, self.a_queries)
+        print("\n--------------------------------\n")
 
 if __name__=='__main__':
-    test("test1.txt")
+    t1 = Test("Test 1", "test1.txt", ["something"], ["someth"])
+    t2 = Test("Test 2", "test2.txt", ["something"], ["someth"])
+    t3 = Test("Test 3", "test3.txt", ["something"], ["someth"])
+
+    t1.conduct()
+    t2.conduct()
+    t3.conduct()
