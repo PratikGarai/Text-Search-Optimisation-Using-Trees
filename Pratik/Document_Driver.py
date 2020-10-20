ba92ce66717186161ec7ee6ffcf66ddbb9dc52c9
PratikGarai <pratikgarai0208@gmail.com>
2020-10-20 09:23:29 +0530

    Added document driver

---
+++ Pratik/Document_Driver.py
@@ -0,0 +1,62 @@
+import Document from Document
+import time
+
+def main():
+
+    FILE_NAME = "test2.txt"
+    doc = None
+
+    # setups
+    with open(FILE_NAME, "r") as t :
+        text = t.read()
+        a = time.time_ns()
+        doc = Document(text)
+        b = time.time_ns()
+        print("Model initialised in : ",(b-a)/10**9,"s")
+
+    # printer methods
+    def search_results_printer(res):
+        if len(res):
+            print("Node exists . Addresses : " )
+            print("S.no.\tW.no.\tBeg.\tEnd")
+            for i in res:
+                print(i[0], i[1], i[2], i[3], sep='\t')
+        else:
+            print("Node doesn't exist")
+
+    def autocomplete_results_printer(res):
+        if res==[]:
+            print("No suggestions")
+        else:
+            print("Num.\tSuggestion")
+            for num, i in enumerate(res):
+                print(num+1,i,sep="\t")
+
+
+    # user inputs
+    print("Enter the number of search queries : ", end="")
+    n_searches  = int(input())
+    for i in range(n_searches):
+        print("Enter the",(i+1),"query : ", end="")
+        q = input()
+        a = time.time_ns()
+        res = doc.serarch(q)
+        b = time.time_ns()
+        print("\nResults :")
+        search_results_printer(res)
+        print("\nResults fetched in :",(b-a),"ns")
+
+    print("Enter the number of autocomplete queries : ", end="")
+    n_searches  = int(input())
+    for i in range(n_searches):
+        print("Enter the",(i+1),"query : ", end="")
+        q = input()
+        a = time.time_ns()
+        res = doc.serarch(q)
+        b = time.time_ns()
+        print("\nResults :")
+        autocomplete_results_printer(res)
+        print("\nResults fetched in :",(b-a),"ns")
+
+if __name__=='__main__':
+    main()
