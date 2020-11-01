19bb758e8437ef69126093e8aa1a89f2f1f1dcd2
ritikasarkar18 <ritikasarkar438@gmail.com>
2020-11-01 17:55:56 +0530

    Added final code for tree

--- Visualising graphs/Graph visualiser.py
+++ Visualising graphs/Graph visualiser.py
@@ -1,13 +1,14 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G=nx.Graph()
+G=nx.DiGraph()
 #edge=("a","b")
 
+#add edge list
 n=int(input('Enter number of edges: '))
 print('Enter edge pairs separated by space: ')
 for i in range(n):
-	edge=tuple(int(x.strip()) for x in input().split())
+	edge=tuple((x.strip()) for x in input().split())
 	G.add_edge(*edge)
 
 #G.add_node("a");
@@ -17,6 +18,6 @@ print(edge)
 
 print("Nodes: ",G.nodes())
 print("Edges: ",G.edges())
-nx.draw(G)
+nx.draw(G, with_labels= True)
 #plt.savefig("graph1.png") #change name of file
 plt.show()  #immediate output
